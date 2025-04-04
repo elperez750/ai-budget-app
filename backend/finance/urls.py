d3abde1e631cfb306b263e2f6bcc67a86807202e
elperez750 <elperez750@gmail.com>
2025-04-03 19:38:14 -0700

    Adding urls for finance

--- backend/finance/urls.py
+++ backend/finance/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('simulate_transactions/', SimulateTransactionsView.as_view()),
     path('webhook/', PlaidWebhookView.as_view(), name='plaid-webhook'),
     path('sync_transactions/', SyncTransactionsView.as_view(), name='sync-transactions'),
-
 ]
